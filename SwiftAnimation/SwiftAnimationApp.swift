0e1723c92ec038b80a86a48c07369d6b74fc6310
dira sanjaya wardana <dirawardana.dw@gmail.com>
2024-04-22 10:35:36 +0700

    Initial Commit

---
+++ SwiftAnimation/SwiftAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAnimationApp.swift
+//  SwiftAnimation
+//
+//  Created by MACBOOK PRO on 22/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
